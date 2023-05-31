45cbde903410d238c12765167d27f7e55a60e5ac
tetianaShev2 <105647410+tetianaShev2@users.noreply.github.com>
2023-05-31 12:30:12 +0200

    added abousUs section

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,29 @@
+import Restaurant from "./img/restauranfood.jpg";
+import "./Main.css";
 function Main() {
-  return <main></main>;
+  return (
+    <main>
+      <article className="aboutUs">
+        <h1>Little Lemon</h1>
+        <h3>Chicago</h3>
+        <section className="description">
+          <p>
+            We are a family owned Mediterranean restaurant, focused on
+            traditional recipes served with a modern twist.
+          </p>
+
+          <img
+            className="restaurantFood"
+            src={Restaurant}
+            width={250}
+            height={152}
+            alt="restaurant food"
+          />
+        </section>
+
+        <button classname="reservation">Reserve a table</button>
+      </article>
+    </main>
+  );
 }
 export default Main;
